83ef31f6871c280556a65dacf36f8bf07a728346
vmie-org <office@stocktech.org>
2025-01-27 20:25:16 +0000

    Create App.vue

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <div id="app">
+    <h1>Hello World</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App'
+};
+</script>
+
+<style>
+#app {
+  text-align: center;
+  margin-top: 100px;
+}
+</style>
